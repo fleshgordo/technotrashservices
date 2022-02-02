eb26ce86e6ed1527a97e6a0f333bc296d18b5c5d
Gordan <robot@yugo.at>
2022-02-02 12:00:56 +0100

    containment

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    var a = 3;
-    $(
-      "#box1,#box2,#box3,#box4,#box5,#box6,#box7,#box8"
-    ).draggable({
-      start: function(event, ui) {
-        $(this).css("z-index", a++);
-      }
+$(document).ready(function () {
+    let a = 3;
+    $("#box1,#box2,#box3,#box4,#box5,#box6,#box7,#box8").draggable({
+        preventCollision: true,
+        containment: ".container-objects",
+        start: function (event, ui) {
+            $(this).css("z-index", a++);
+        }
     });
     //   $('#dragZone div').click(function() {
     //     $(this).addClass('top').removeClass('bottom');
@@ -13,4 +13,4 @@ $(document).ready(function() {
     //     $(this).css("z-index", a++);
 
     //   });
-  });
+});

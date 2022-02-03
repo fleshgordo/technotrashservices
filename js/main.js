200c3220c81b54ac2710b5433ef92f7ca25cf4e7
Gordan <robot@yugo.at>
2022-02-03 09:25:27 +0100

    random positioning update

--- js/main.js
+++ js/main.js
@@ -12,20 +12,30 @@ $(document).ready(function () {
     let random_pos_y = [];
     let number_of_elements = 8;
     // generate array with equal positioning on width and height
-    for (let i=1; i<number_of_elements;i++){
+    for (let i = 0; i < number_of_elements; i++) {
         let offset = 50;
-        random_pos_x.push(Math.floor(i * window.innerWidth/number_of_elements))
-        random_pos_y.push(Math.floor(i * window.innerHeight/number_of_elements - offset))
+        random_pos_x.push(Math.floor(i * window.innerWidth / number_of_elements))
+        random_pos_y.push(Math.floor(i * window.innerHeight / number_of_elements - offset))
     }
     // shuffle position array
     random_pos_x.sort(() => (Math.random() > .5) ? 1 : -1);
     random_pos_y.sort(() => (Math.random() > .5) ? 1 : -1);
     // random positioning of objects
     $(".box").each(element => {
+
         let random_rotate = 15;
-        let x = random_pos_x[element-1]
-        let y = random_pos_y[element-1] 
-        let random_deg = random_rotate * .5 - Math.floor(Math.random()*random_rotate)
-        $("#box"+element).css({top: y + "px", left: x + "px", position:'absolute', transform: "rotate("+random_deg+"deg"});
+        let x = random_pos_x[element]
+        let y = random_pos_y[element]
+        element += 1;
+        y > window.innerHeight - 400 ? y -  $("#box" + element).css("height") : y;
+        x > window.innerWidth - 400 ? y -  $("#box" + element).css("width") : y;
+        console.log(y)
+        let random_deg = random_rotate * .5 - Math.floor(Math.random() * random_rotate)
+        $("#box" + element).css({
+            top: y + "px",
+            left: x + "px",
+            position: 'absolute',
+            transform: "rotate(" + random_deg + "deg"
+        });
     });
 });
